745b1acb653fb690b3dff4258db19ae47205a702
i-steve <mickyvishnu007@gmail.com>
2022-06-15 00:41:19 +0530

    Initial Commit

---
+++ BMI/Control/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Vishnu Priyan on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var calculateBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    var h :Float = 1.5
+    var w :Int = 100
+    
+    @IBAction func heightChange(_ sender: UISlider) {
+        h = Float(String(format: "%.2f", sender.value))!
+        heightLabel.text = "\(h)m"
+    }
+    
+    @IBAction func weightChange(_ sender: UISlider) {
+        w = Int(sender.value)
+        weightLabel.text = "\(w)kg"
+    }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        let bmi = bmiCalc(h,w)
+        textLabel.text = "Your Result \n \(String(format: "%.1f", bmi))"
+    }
+    
+}
+
---
+++ BMI/Model/BMI Calculator.swift
@@ -0,0 +1,12 @@
+//
+//  BMI Calculator.swift
+//  BMI
+//
+//  Created by Vishnu Priyan on 15/06/22.
+//
+
+import Foundation
+
+func bmiCalc(_ h:Float, _ w:Int) -> Float{
+    return Float(w)/(h*h)
+}
